b7bd310f9f0d420acf309d9ce7d8f2005ecf5f28
romanfornessi <123643278+romanfornessi@users.noreply.github.com>
2023-01-26 10:13:48 -0300

    Add files via upload

---
+++ TFI2.js
@@ -0,0 +1,10 @@
+document.getElementById("botón").onclick = function ( ) {
+    console.log ("Capturamos el evento click"); 
+    document.getElementById ("demo").innerHTML = "Linkedin: Román Fornessi   Instagram: Román Fornessi"
+}
+
+window.alert("Antes de ver mi CV me gustaría avisarte que estaré de vacaciones hasta el 20 de febrero, por lo que, si me escribís, podré ver tu mensaje ese día :)");
+
+function myFunction() {
+    document.getElementById("frm1").submit();
+  }
